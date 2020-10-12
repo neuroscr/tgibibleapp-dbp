60163d7a9f09f8fe7702b1660d9992d49d86efde
Casey Justus <cjustus@keyholesoftware.com>
2020-10-12 13:55:00 -0500

    iterate through testkey csv

--- tools/permissionValidation/permission.py
+++ tools/permissionValidation/permission.py
@@ -9,7 +9,7 @@ import config
 from myconfig import *
 # you need to add a myconfig.py file
 
-singlekey = 'testkey-135';
+singlekey = '18459gba89ga94tha84dbg98ba98';
 apiUrl = 'http://api.dbp.test:80/api/';
 
 
@@ -31,19 +31,21 @@ def keyTest(mykey):
         cursor = cnx.cursor()
 
         query = ("SELECT agak.access_group_id, bf.id, bf.asset_id, bf.set_type_code, bf.set_size_code "
-                "FROM dbp_200909.access_group_filesets agf "
-                "JOIN  dbp_200909.bible_filesets bf ON agf.hash_id=bf.hash_id "
+#                "FROM dbp_200909.access_group_filesets agf "
+#                "JOIN  dbp_200909.bible_filesets bf ON agf.hash_id=bf.hash_id "
+                "FROM access_group_filesets agf "
+                "JOIN bible_filesets bf ON agf.hash_id=bf.hash_id "
                 "JOIN dbp_users.access_group_api_keys agak ON agak.access_group_id = agf.access_group_id "
                 "JOIN dbp_users.user_keys uk ON agak.key_id=uk.id "
                 "WHERE uk.key IN ('" + mykey + "') "
                 "ORDER BY bf.id")
 
-        #print(query)
+#        print(query)
 
         cursor.execute(query)
         myresult = cursor.fetchall()
 
-       # print(myresult)
+#        print(myresult)
     
         return myresult
         cursor.close()
@@ -55,10 +57,10 @@ def getFlattenedApi(mykey):
     url = apiUrl + 'bibles?key='+ mykey + '&v=4' ;
     if mykey.__contains__('5'):
         url = url + '&asset_id=dbp-vid'
+#    print(url)
     response = urlopen(url)
     apiResult = json.load(response)
     newApi = []
-    #print(apiResult)
     for el in apiResult['data']:
         
         try:
@@ -77,8 +79,6 @@ def getFlattenedApi(mykey):
         except KeyError:
             pass # doesn't exist
 
-    #print(newApi)
-    print(len(newApi))
     return newApi
 
 
@@ -90,52 +90,35 @@ def compareFilesets(mykey):
     #fmtApiResult = json.dumps(apiResult, indent=2)
 
     dbResult = keyTest(mykey);
-    #print(dbResult)
     dbFilesets = len(dbResult)
 
     if apiNum == dbFilesets:
         print(mykey + ' has the same number of filesets ' + str(apiNum))
     else:
-        print(mykey + ' filesets returned by api: ' + str(apiNum))
-        print(mykey + ' filesets returned by db: ' + str(dbFilesets))
+        print(mykey + ' filesets returned by api: ' + str(apiNum) + ', by db: ' + str(dbFilesets))
 
-
-    with open('permissions-'+mykey+'-db.csv', 'w', newline='') as csvfile:
+    with open('permissions/permissions-'+mykey+'-db.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['access_group_id', 'id', 'asset_id', 'set_type_code', 'set_type_size', 'found_in_api'])
         
         for dbRow in dbResult:
 
+            dbRowList = []
             for apiEl in apiList:
                 dbRowList = list(dbRow)
 
                 if (apiEl['id'][0:6] == dbRow[1][0:6] and apiEl['asset'] == dbRow[2] and apiEl['type'] == dbRow[3] and apiEl['size'] == dbRow[4]):
                     dbRowList.extend(['yes'])
-                    #print(dbRowList)
-                    #print(apiEl)                    
-                    #print('yes')
                     break
                 else:
                     dbRowList.extend(['no']) 
-                    # print('no')
 
             writer.writerow(dbRowList)
-            #print(dbRowList)
         
-  
-# iterate thru spreadsheet keys
-# loc = ("permissionTest.xlsx") 
-# wb = xlrd.open_workbook(loc) 
-# sheet = wb.sheet_by_index(0) 
- 
+with open('testkeys.csv', newline='') as csvfile:
+    keyreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in keyreader:
+        compareFilesets(row[0])
 
-
-# columns = sheet.col(1)
-# columns.pop(0)#removed heading
-
-
-# for keyval in columns:
-#     mykey = keyval.value
-#    print('key ' + mykey)
-#    compareFilesets(mykey)
-compareFilesets(singlekey)
+#single file        
+#compareFilesets(singlekey)

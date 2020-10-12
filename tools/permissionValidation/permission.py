7ea47805b7f7e1d875567b4b2a37dfc2f731d4b0
Casey Justus <cjustus@keyholesoftware.com>
2020-10-12 11:19:50 -0500

    added dbp-vid asset type

--- tools/permissionValidation/permission.py
+++ tools/permissionValidation/permission.py
@@ -9,7 +9,7 @@ import config
 from myconfig import *
 # you need to add a myconfig.py file
 
-singlekey = 'somekey';
+singlekey = 'testkey-135';
 apiUrl = 'http://api.dbp.test:80/api/';
 
 
@@ -31,19 +31,19 @@ def keyTest(mykey):
         cursor = cnx.cursor()
 
         query = ("SELECT agak.access_group_id, bf.id, bf.asset_id, bf.set_type_code, bf.set_size_code "
-                "FROM access_group_filesets agf "
-                "JOIN bible_filesets bf ON agf.hash_id=bf.hash_id "
+                "FROM dbp_200909.access_group_filesets agf "
+                "JOIN  dbp_200909.bible_filesets bf ON agf.hash_id=bf.hash_id "
                 "JOIN dbp_users.access_group_api_keys agak ON agak.access_group_id = agf.access_group_id "
                 "JOIN dbp_users.user_keys uk ON agak.key_id=uk.id "
                 "WHERE uk.key IN ('" + mykey + "') "
                 "ORDER BY bf.id")
 
-        print(query)
+        #print(query)
 
         cursor.execute(query)
         myresult = cursor.fetchall()
 
-        print(myresult)
+       # print(myresult)
     
         return myresult
         cursor.close()
@@ -52,16 +52,30 @@ def keyTest(mykey):
     cnx.close()
 
 def getFlattenedApi(mykey):
-    response = urlopen(apiUrl + 'bibles?key='+ mykey + '&v=4')
+    url = apiUrl + 'bibles?key='+ mykey + '&v=4' ;
+    if mykey.__contains__('5'):
+        url = url + '&asset_id=dbp-vid'
+    response = urlopen(url)
     apiResult = json.load(response)
     newApi = []
     #print(apiResult)
     for el in apiResult['data']:
         
-        if el['filesets']['dbp-prod']:
-            for fs in el['filesets']['dbp-prod']:
-                fs['asset'] = 'dbp-prod'
-                newApi.append(fs)
+        try:
+            if el['filesets']['dbp-vid']:
+                for fs in el['filesets']['dbp-vid']:
+                    fs['asset'] = 'dbp-vid'
+                    newApi.append(fs)
+        except KeyError:
+            pass   # doesn't exist
+
+        try:
+            if el['filesets']['dbp-prod']:
+                for fs in el['filesets']['dbp-prod']:
+                    fs['asset'] = 'dbp-prod'
+                    newApi.append(fs)
+        except KeyError:
+            pass # doesn't exist
 
     #print(newApi)
     print(len(newApi))
@@ -86,7 +100,7 @@ def compareFilesets(mykey):
         print(mykey + ' filesets returned by db: ' + str(dbFilesets))
 
 
-    with open('permissions-testkey-101-db.csv', 'w', newline='') as csvfile:
+    with open('permissions-'+mykey+'-db.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['access_group_id', 'id', 'asset_id', 'set_type_code', 'set_type_size', 'found_in_api'])
         
@@ -94,31 +108,34 @@ def compareFilesets(mykey):
 
             for apiEl in apiList:
                 dbRowList = list(dbRow)
+
                 if (apiEl['id'][0:6] == dbRow[1][0:6] and apiEl['asset'] == dbRow[2] and apiEl['type'] == dbRow[3] and apiEl['size'] == dbRow[4]):
                     dbRowList.extend(['yes'])
                     #print(dbRowList)
-                    #print(apiEl)
+                    #print(apiEl)                    
+                    #print('yes')
+                    break
                 else:
                     dbRowList.extend(['no']) 
-                    
+                    # print('no')
 
             writer.writerow(dbRowList)
             #print(dbRowList)
         
   
 # iterate thru spreadsheet keys
-loc = ("permissionTest.xlsx") 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
+# loc = ("permissionTest.xlsx") 
+# wb = xlrd.open_workbook(loc) 
+# sheet = wb.sheet_by_index(0) 
  
 
 
-columns = sheet.col(1)
-columns.pop(0)#removed heading
+# columns = sheet.col(1)
+# columns.pop(0)#removed heading
 
 
-for keyval in columns:
-    mykey = keyval.value
+# for keyval in columns:
+#     mykey = keyval.value
 #    print('key ' + mykey)
 #    compareFilesets(mykey)
 compareFilesets(singlekey)

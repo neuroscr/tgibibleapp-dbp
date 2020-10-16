3cea7fab8b32d4f02f53b0bd08f05f6340256e51
Jose Ramirez <joseramirez@Jose-Ramirez-MacBook-Pro.local>
2020-10-16 14:39:47 -0500

    FCBH-2599 Create translate playlist artisan command * Create route for artisan command * translate playlist using playlistController * Sync duration and verses

--- app/Console/Commands/translatePlaylist.php
+++ app/Console/Commands/translatePlaylist.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\Playlist\PlaylistsController;
 use App\Models\Language\Language;
 use App\Models\Playlist\Playlist;
 use App\Models\Playlist\PlaylistItems;
@@ -63,55 +64,66 @@ class translatePlaylist extends Command
 
 
         $playlist = Playlist::where('id', $playlist_id)->first();
-        echo($playlist);
-        $this->alert('Translating playlist ' . $playlist->name . ' starting: ' . Carbon::now());
-        /*if (!$playlist) {
+        if (!$playlist) {
             $this->error('Playlist with ID:' . $playlist_id . ' does not exist');
         } else {
+            $this->alert('Translating playlist ' . $playlist->name . ' starting: ' . Carbon::now());
             $bible_ids = explode(',', $bible_ids);
             $user = User::with('projects')->whereId($playlist->user_id)->first();
+            $playlist_controller = new MessagedPlaylistController();
             foreach ($bible_ids as $key => $bible_id) {
                 $request = new Request();
                 request()->merge(['bible_id' => $bible_id]);
                 try {
-                    $this->line('Translating plan to bible ' . $bible_id . ' started ' . Carbon::now());
-                    $new_plan = $plan_controller->translate($request, $plan_id, $user, false, false);
-                    $plan = Plan::where('id', $new_plan['id'])->first();
+                    $this->line('Translating playlist to bible ' . $bible_id . ' started ' . Carbon::now());
+                    $translated_playlist = $playlist_controller->translate($request, $playlist_id, $user, false)->getData();
+                    $playlist = Playlist::where('id', $translated_playlist->id)->first();
                     $language_id = DB::connection('dbp')
                         ->table('bibles')
                         ->select(['language_id'])
                         ->whereId($bible_id)->first();
 
-                    $plan->language_id = $language_id->language_id;
-                    $plan->save();
+                    $playlist->language_id = $language_id->language_id;
+                    $playlist->save();
 
-                    $this->line('Calculating duration and verses ' . Carbon::now());
-                    foreach ($plan->days as $day) {
-                        $playlist_items = PlaylistItems::where('playlist_id', $day['playlist_id'])->get();
-                        foreach ($playlist_items as $playlist_item) {
-                            $playlist_item->calculateDuration()->save();
-                            $playlist_item->calculateVerses()->save();
-                        }
+                    $playlist_items = PlaylistItems::when($playlist_id, function ($query, $playlist_id) {
+                        $query->where('playlist_id', $playlist_id);
+                    })->get();
+                    $this->line(Carbon::now() . ' Sync starting for ' . sizeof($playlist_items) . ' items');
+                    foreach ($playlist_items as $key => $playlist_item) {
+                        $this->line(Carbon::now() . ' Calculating duration and verses of item ' . ($key + 1) . ' started');
+                        $playlist_item->calculateDuration()->save();
+                        $playlist_item->calculateVerses()->save();
+                        $this->info(Carbon::now() . ' Calculating duration and verses of item ' . ($key + 1) . ' finalized');
+                        $this->line('');
                     }
 
-                    $this->info('Translating plan to bible ' . $bible_id . ' finalized ' . Carbon::now());
+                    $this->info('Translating playlist to bible ' . $bible_id . ' finalized ' . Carbon::now());
                     $this->line('');
                 } catch (Exception $e) {
-                    $this->error('Error translating plan to bible ' . $bible_id . ' ');
+                    $this->error('Error translating playlist to bible ' . $bible_id . ' ');
                     $this->error('Error message: ' . $e->getMessage() . ' ');
                     $this->error('Error timestamp: ' . Carbon::now() . ' ');
                     $this->line('');
-                    $this->question('Please fix the issue translating the plan to ' . $bible_id . ' ');
+                    $this->question('Please fix the issue translating the playlisy to ' . $bible_id . ' ');
                     $this->question('To continue the process please run the following command: ');
                     $this->line('');
 
-                    $this->comment("\t<fg=green>php artisan translate:plan " . $plan_id . ' ' . implode(',', array_splice($bible_ids, $key)));
+                    $this->comment("\t<fg=green>php artisan translate:playlisy " . $playlist_id . ' ' . implode(',', array_splice($bible_ids, $key)));
                     break;
                 }
             }
-        }*/
+        }
 
         $this->line('');
-        $this->alert('Translating plan end: ' . Carbon::now());
+        $this->alert('Translating playlist end: ' . Carbon::now());
+    }
+}
+
+class MessagedPlaylistController extends PlaylistsController
+{
+    public function replyWithError($message, $action = null)
+    {
+        throw (new Exception($message));
     }
 }

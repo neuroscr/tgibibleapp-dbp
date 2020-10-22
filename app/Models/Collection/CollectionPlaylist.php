d929830e9f591127fc2cf11bdae05257a784ee54
Ryan Tharp <neuro@interx.net>
2020-10-22 04:27:15 +0000

    set up db/table and namespace

--- app/Models/Collection/CollectionPlaylist.php
+++ app/Models/Collection/CollectionPlaylist.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models\Collection;
 
 use Illuminate\Database\Eloquent\Model;
 
 class CollectionPlaylist extends Model
 {
-    //
+    protected $connection = 'dbp_users';
+    public $table         = 'collection_playlists';
 }

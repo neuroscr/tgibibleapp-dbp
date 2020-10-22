a434b9bded7caff6db404b3a3554ad9c10143128
Ryan Tharp <neuro@interx.net>
2020-10-22 04:11:40 +0000

    include user relation

--- app/Models/Collection/Collection.php
+++ app/Models/Collection/Collection.php
@@ -3,10 +3,16 @@
 namespace App\Models\Collection;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User\User;
 
 class Collection extends Model
 {
     protected $connection = 'dbp_users';
     public $table         = 'collections';
     //
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->select('id', 'name');
+    }
 }

8f15d51e6c72f57adc78f2c7d0e08c7d4436f6c7
Ryan Tharp <neuro@interx.net>
2020-10-23 03:32:49 +0000

    add collection playlist

--- database/factories/CollectionFactory.php
+++ database/factories/CollectionFactory.php
@@ -4,8 +4,10 @@ use Faker\Generator as Faker;
 
 use App\Models\User\User;
 use App\Models\Collection\Collection;
+use App\Models\Collection\CollectionPlaylist;
+use App\Models\Playlist\Playlist;
 
-// Project Factory
+// Collection Factory
 $factory->define(Collection::class, function (Faker $faker) {
     return [
         // this is auto increment, we don't need to set it
@@ -13,6 +15,18 @@ $factory->define(Collection::class, function (Faker $faker) {
         //'id'               => random_int(0, 9999),
         //'id'               => factory(User::class)->create()->id,
         'name'             => $faker->name,
-        //'deleted_at'       => null
+    ];
+});
+
+// Collection Playlist Factory
+$factory->define(CollectionPlaylist::class, function (Faker $faker) {
+    return [
+        // this is auto increment, we don't need to set it
+        // random can cause conflicts
+        //'id'               => factory(User::class)->create()->id,
+        'collection_id'              => factory(Collection::class)->create()->id,
+        //'playlist_id'                => factory(Playlist::class)->create()->id,
+        'playlist_id'                => 4,
+        'order_column'               => random_int(0, 9999),
     ];
 });

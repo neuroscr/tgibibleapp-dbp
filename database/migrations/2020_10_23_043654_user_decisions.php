cecced0c76d3e80e82f603304a3ac243ac884b04
Ryan Tharp <neuro@interx.net>
2020-10-23 06:40:09 +0000

    migration

---
+++ database/migrations/2020_10_23_043654_user_decisions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserDecisions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('dbp_users')->table('users', function (Blueprint $table) {
+            $table->string('decision_name')->after('remember_token')->nullable()->default(null);
+            $table->integer('decision_date')->after('decision_name')->unsigned()->nullable()->default(null);
+            $table->boolean('decision_want_resources')->after('decision_date')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('dbp_users')->table('users', function (Blueprint $table) {
+            $table->dropColumn('decision_name');
+            $table->dropColumn('decision_date');
+            $table->dropColumn('decision_want_resources');
+        });
+    }
+}

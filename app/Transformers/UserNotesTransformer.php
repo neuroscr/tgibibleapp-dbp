79eb226c41a03ed8eb77990ae282b73c66666032
Ryan Tharp <neuro@interx.net>
2020-10-27 02:48:16 +0000

    lookup bible/book if there's a content server

--- app/Transformers/UserNotesTransformer.php
+++ app/Transformers/UserNotesTransformer.php
@@ -4,6 +4,7 @@ namespace App\Transformers;
 
 use App\Models\User\Study\Note;
 use League\Fractal\TransformerAbstract;
+use GuzzleHttp\Client;
 
 class UserNotesTransformer extends TransformerAbstract
 {
@@ -46,20 +47,46 @@ class UserNotesTransformer extends TransformerAbstract
      */
     public function transform(Note $note)
     {
+        $content_config = config('services.content');
+        if (empty($content_config['url'])) {
+            $book_name = optional($note->book)->name;
+            $bible_name = $note->bible_name;
+        } else {
+            // getBibleNameAttribute
+            $bible_name = cacheRemember('bible_name', [$note->bible_id, $GLOBALS['i18n_id']], now()->addDay(), function () use ($note, $content_config) {
+                $client = new Client();
+                echo "Looking up<br>\n";
+                $res = $client->get($content_config['url'] . 'bibles/' . $note->bible_id
+                  . '/name/' . $GLOBALS['i18n_id'] . '?v=4&key=' . $content_config['key']);
+                return $res->getBody().'';
+            });
+            // book
+            $book_name = cacheRemember('book_data', [$note->bible_id, $note->book_id], now()->addDay(), function () use ($note, $content_config) {
+                $client = new Client();
+                $res = $client->get($content_config['url'] . 'bibles/' . $note->bible_id .
+                  '/book/' . $note->book_id . '?v=4&key=' . $content_config['key']);
+                $book_data = json_decode($res->getBody() . '');
+                $book = '';
+                if ($book_data && $book_data->data &&count($book_data->data)) {
+                  $book = $book_data->data[0]->name;
+                }
+                return $book;
+            });
+        }
         return [
-      'id' => (int) $note->id,
-      'bible_id' => (string) $note->bible_id,
-      'bible_name' => (string) $note->bible_name,
-      'book_id' => (string) $note->book_id,
-      'book_name' => (string) optional($note->book)->name,
-      'chapter' => (int) $note->chapter,
-      'verse_start' => (int) $note->verse_start,
-      'verse_end' => (int) $note->verse_end,
-      'verse_text' => (string) $note->verse_text,
-      'notes' => (string) $note->notes,
-      'created_at' => (string) $note->created_at,
-      'updated_at' => (string) $note->updated_at,
-      'tags' => $note->tags
-    ];
+          'id' => (int) $note->id,
+          'bible_id' => (string) $note->bible_id,
+          'bible_name' => (string) $bible_name,
+          'book_id' => (string) $note->book_id,
+          'book_name' => (string) $book_name,
+          'chapter' => (int) $note->chapter,
+          'verse_start' => (int) $note->verse_start,
+          'verse_end' => (int) $note->verse_end,
+          'verse_text' => (string) $note->verse_text,
+          'notes' => (string) $note->notes,
+          'created_at' => (string) $note->created_at,
+          'updated_at' => (string) $note->updated_at,
+          'tags' => $note->tags
+        ];
     }
 }

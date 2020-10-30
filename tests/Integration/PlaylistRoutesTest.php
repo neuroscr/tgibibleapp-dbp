18297161f0387f6a786ff3768072d64403b71c8f
Ryan Tharp <neuro@interx.net>
2020-10-30 05:49:08 +0000

    add hls/itemHls, update other unit tests

--- tests/Integration/PlaylistRoutesTest.php
+++ tests/Integration/PlaylistRoutesTest.php
@@ -18,17 +18,64 @@ class PlaylistRoutesTest extends ApiV4Test
      */
     public function show()
     {
-        //
-        $test_playlist_id = 472982;
+        // just hard code for now
+        $test_playlist_id = 875;
         $path = route('v4_playlists.show', array_merge($this->params, [
             'playlist_id'    => $test_playlist_id,
         ]));
         echo "\nTesting: $path";
         $response = $this->withHeaders($this->params)->get($path);
+        $result = json_decode($response->getContent(), true);
         $response->assertSuccessful();
-        $result = collect(json_decode($response->getContent()));
-        $this->assertEquals($result->count(), 13); // has 13 fields...
-        $this->assertEquals($result['id'], $test_playlist_id);
+        $this->assertEquals(13, count($result)); // has 13 fields...
+        $this->assertEquals($test_playlist_id, $result['id']);
+        $this->assertEquals(19, count($result['items']));
         //$this->assertEquals($result->user->id, '1223628');
     }
+
+    /**
+     * @category V4_API
+     * @category Route Name: v4_playlists.hls
+     * @category Route Path: https://api.dbp.test/playlists/{playlist_id}/hls?v=4&key={key}
+     * @see      PlaylistsController::hls
+     * @group    V4
+     * @group    travis
+     * @test
+     */
+    public function hls()
+    {
+        // just hard code for now
+        $test_playlist_id = 875;
+        $path = route('v4_playlists.hls', array_merge($this->params, [
+            'playlist_id'    => $test_playlist_id,
+        ]));
+        echo "\nTesting: $path";
+        $response = $this->withHeaders($this->params)->get($path);
+        $result = json_decode($response->getContent());
+        $response->assertSuccessful();
+    }
+
+    /**
+     * @category V4_API
+     * @category Route Name: v4_playlists_item.hls
+     * @category Route Path: https://api.dbp.test/playlists/{playlist_item_id}/item-hls?v=4&key={key}
+     * @see      PlaylistsController::itemHls
+     * @group    V4
+     * @group    travis
+     * @test
+     */
+    public function itemHls()
+    {
+        // just hard code for now
+        $test_playlist_item_id = 107078; // part of 875
+        $path = route('v4_playlists_item.hls', array_merge($this->params, [
+            'playlist_item_id'    => $test_playlist_item_id,
+        ]));
+        echo "\nTesting: $path";
+        $response = $this->withHeaders($this->params)->get($path);
+        $result = json_decode($response->getContent());
+        $response->assertSuccessful();
+    }
+
+
 }

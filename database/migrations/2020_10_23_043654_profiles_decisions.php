831cb429aa9910d98f5dc660997553366cc40135
Ryan Tharp <neuro@interx.net>
2020-11-03 15:23:10 -0800

    GID-73 | Add decision fields in dbp_users.profiles table (#6)
    * fix API_URL_DOCS already defined in unit tests
    
    * migration
    
    * add decision fields
    
    * add decision fields
    
    * make as many tests work as time permitting
    
    * add decision fields to store/show
    
    * get enough store/update working to verify decision fields are committing
    
    * Change date from uint to string per Johan
    
    * update decision_date type to string
    
    * update decision_date
    
    * rename migration fro m users => profiles
    
    * place fields in profiles instead of user
    
    * remove decision fields
    
    * remove decision fields
    
    * add decision fields
    
    * remove decision fields
    
    * remove decision annotations
    
    * remove decision fields

---
+++ database/migrations/2020_10_23_043654_profiles_decisions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserDecisions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('dbp_users')->table('profiles', function (Blueprint $table) {
+            $table->string('decision_name')->after('phone')->nullable()->default(null);
+            $table->string('decision_date')->after('decision_name')->nullable()->default(null);
+            $table->boolean('decision_want_resources')->after('decision_date')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('dbp_users')->table('profiles', function (Blueprint $table) {
+            $table->dropColumn('decision_name');
+            $table->dropColumn('decision_date');
+            $table->dropColumn('decision_want_resources');
+        });
+    }
+}

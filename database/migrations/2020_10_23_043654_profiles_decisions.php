42b7e594c8a3fb5de1daca8d1daaedada3b28d85
Ryan Tharp <neuro@interx.net>
2020-11-04 09:23:37 -0800

    GID-73 | migration fix (#21)
    * fix API_URL_DOCS already defined in unit tests
    
    * migration
    
    * add decision fields
    
    * add decision fields
    
    * make as many tests work as time permitting
    
    * add decision fields to store/show
    
    * get enough store/update working to verify decision fields are committing
    
    * Change date from uint to string per Johan
    
    * update decision_date type to string
    
    * update decision_date
    
    * rename migration fro m users => profiles
    
    * place fields in profiles instead of user
    
    * remove decision fields
    
    * remove decision fields
    
    * add decision fields
    
    * remove decision fields
    
    * remove decision annotations
    
    * remove decision fields
    
    * fix class name to match file, so you can apply the migration

--- database/migrations/2020_10_23_043654_profiles_decisions.php
+++ database/migrations/2020_10_23_043654_profiles_decisions.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UserDecisions extends Migration
+class ProfilesDecisions extends Migration
 {
     /**
      * Run the migrations.

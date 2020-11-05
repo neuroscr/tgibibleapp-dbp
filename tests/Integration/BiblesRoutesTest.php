c930aafe49b0a75e67a7bc9408f35b01f5d1df22
Ryan Tharp <neuro@interx.net>
2020-11-05 12:51:27 -0800

    GID-151 | [content-side] Notes (#8)
    * bookSearch/oneName for v4_bible
    
    * bookSearch()/showName()
    
    * bookVerse()
    
    * bookVerse()
    
    * mark filesetbooks incomplete, add basic bookSearch/bookVerse/oneName checks
    
    * cache showName, remove access check

--- tests/Integration/BiblesRoutesTest.php
+++ tests/Integration/BiblesRoutesTest.php
@@ -109,7 +109,7 @@ class BiblesRoutesTest extends ApiV4Test
         $this->markTestIncomplete('Book transformer needs fix');
         $params = array_merge(['fileset_id' => 'ENGESV', 'fileset_type' => 'text_plain'], $this->params);
         $path = route('v4_filesets.books', $params);
-        echo "\nTesting: $path";
+        echo "\nTesting: GET $path";
 
         $response = $this->withHeaders($this->params)->get($path);
         $response->assertSuccessful();
@@ -235,14 +235,74 @@ class BiblesRoutesTest extends ApiV4Test
         $response->assertSuccessful();
         $result = json_decode($response->getContent(), true);
         // should have 2 keys: language and audio
-        $this->assertEquals(count($result), 2);
+        $this->assertEquals(2, count($result));
         // language check
-        $this->assertEquals($result['language'], 'Bomu');
+        $this->assertEquals('Bomu', $result['language']);
         // audio check
-        $this->assertEquals(collect($result['audio'])->count(), 4);
+        $this->assertEquals(3, collect($result['audio'])->count());
     }
 
 
+    /**
+     * @category V4_API
+     * @category Route Name: v4_bible.bookSearch
+     * @category Route Path: https://api.dbp.test/bibles/book/search/Roman?v=4&key={key}
+     * @see      \App\Http\Controllers\Bible\BibleFileSetsController::bookSearch
+     * @group    BibleRoutes
+     * @group    V4
+     * @group    non-travis
+     * @test
+     */
+    public function bibleBookSearch()
+    {
+        $params = array_merge(['query' => 'e'], $this->params);
+        $path = route('v4_bible.bookSearch', $params);
+        echo "\nTesting: GET $path";
+
+        $response = $this->withHeaders($this->params)->get($path);
+        $response->assertSuccessful();
+    }
+
+    /**
+     * @category V4_API
+     * @category Route Name: v4_bible.bookVerse
+     * @category Route Path: https://api.dbp.test/bibles/BIBLE_ID/book/BOOK_ID/CHATPER/VERSE_START?v=4&key={key}
+     * @see      \App\Http\Controllers\Bible\BibleFileSetsController::bookVerse
+     * @group    BibleRoutes
+     * @group    V4
+     * @group    non-travis
+     * @test
+     */
+    public function bibleBookVerse()
+    {
+        $params = array_merge(['bible_id'=>'ENGESV', 'book_id'=>'ROM', 'chapter'=>6, 'verse_start'=>23], $this->params);
+        $path = route('v4_bible.bookVerse', $params);
+        echo "\nTesting: GET $path";
+
+        $response = $this->withHeaders($this->params)->get($path);
+        $response->assertSuccessful();
+    }
+
+
+    /**
+     * @category V4_API
+     * @category Route Name: v4_bible.oneName
+     * @category Route Path: https://api.dbp.test/bibles/BIBLE_ID/name/LANGUAGE?v=4&key={key}
+     * @see      \App\Http\Controllers\Bible\BibleFileSetsController::bookVerse
+     * @group    BibleRoutes
+     * @group    V4
+     * @group    non-travis
+     * @test
+     */
+    public function bibleShowName()
+    {
+        $params = array_merge(['bible_id'=>'ENGESV', 'language'=>6414], $this->params);
+        $path = route('v4_bible.oneName', $params);
+        echo "\nTesting: GET $path";
+
+        $response = $this->withHeaders($this->params)->get($path);
+        $response->assertSuccessful();
+    }
 
     /**
      * @category V4_API

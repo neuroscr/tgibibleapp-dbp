2e9ea2acf90178862d3976516e2f04019b5b7851
Ryan Tharp <neuro@interx.net>
2020-11-06 03:06:34 +0000

    remove duplicate functions, assert annotations()

--- tests/Integration/BiblesRoutesTest.php
+++ tests/Integration/BiblesRoutesTest.php
@@ -326,48 +326,6 @@ class BiblesRoutesTest extends ApiV4Test
         $response->assertSuccessful();
     }
 
-
-    /**
-     * @category V4_API
-     * @category Route Name: v4_bible.bookSearch
-     * @category Route Path: https://api.dbp.test/bibles/book/search/Roman?v=4&key={key}
-     * @see      \App\Http\Controllers\Bible\BibleFileSetsController::bookSearch
-     * @group    BibleRoutes
-     * @group    V4
-     * @group    non-travis
-     * @test
-     */
-    public function bibleBookSearch()
-    {
-        $params = array_merge(['query' => 'e'], $this->params);
-        $path = route('v4_bible.bookSearch', $params);
-        echo "\nTesting: GET $path";
-
-        $response = $this->withHeaders($this->params)->get($path);
-        $response->assertSuccessful();
-    }
-
-    /**
-     * @category V4_API
-     * @category Route Name: v4_bible.bookVerse
-     * @category Route Path: https://api.dbp.test/bibles/BIBLE_ID/book/BOOK_ID/CHATPER/VERSE_START?v=4&key={key}
-     * @see      \App\Http\Controllers\Bible\BibleFileSetsController::bookVerse
-     * @group    BibleRoutes
-     * @group    V4
-     * @group    non-travis
-     * @test
-     */
-    public function bibleBookVerse()
-    {
-        $params = array_merge(['bible_id'=>'ENGESV', 'book_id'=>'ROM', 'chapter'=>6, 'verse_start'=>23], $this->params);
-        $path = route('v4_bible.bookVerse', $params);
-        echo "\nTesting: GET $path";
-
-        $response = $this->withHeaders($this->params)->get($path);
-        $response->assertSuccessful();
-    }
-
-
     /**
      * @category V4_API
      * @category Route Name: v4_bible.oneName
@@ -559,8 +517,10 @@ class BiblesRoutesTest extends ApiV4Test
           'api_token' => 'IRSooPKAWU5dUeEVw6W2rQy3o6ursYtbjMGSeLjljcDSUjopSbEXXIBweli7'
         ], $this->params);
         $path = route('v4_bible.chapter.annotations', $params);
+        $response = $this->withHeaders($this->params)->get($path);
+        $response->assertSuccessful();
     }
-  
+
    /**
      * @category Route Name: v4_bible.bibleVerses
      * @category Route Path: https://api.dbp.test/bibles/X/verses?v=4&key={key}
